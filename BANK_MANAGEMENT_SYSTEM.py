aa4c34f491c5028eb7c697f04c26668bfbc4637f
himanshu <himanshukumar75655893@gmail.com>
2023-10-03 22:00:04 +0530

    renamed the bank_mgmt_system

---
+++ BANK_MANAGEMENT_SYSTEM.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    
+    print('''
+            1. OPEN NEW ACCOUNT
+            2. DEPOSITE AMOUNT 
+            3. WITHDRAW AMOUNT
+            4. BALANCE ENQUIRY
+            5. DISPLAY CUSTOMER DETAILS
+            6. CLOSE AN ACCOUNT
+            ''')
